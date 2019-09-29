a0a0ebc5c230643a521ab2b46ced243d5c58152b
manoj <manojvirat457@gmail.com>
2019-09-29 10:15:40 +0530

    resume download

--- src/app/interfaces/posts.ts
+++ src/app/interfaces/posts.ts
@@ -4,7 +4,7 @@ export class Posts {
     link: string;
     imageLink: string;
 
-    constructor(title: string, pubDate: string, link: string, imageLink: string){
+    constructor(title: string, pubDate: string, link: string, imageLink: string) {
         this.title = title;
         this.pubDate = pubDate;
         this.link = link;
